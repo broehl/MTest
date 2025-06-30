d100b611bc22d6014e66136397750cbed496d024
Bernie Roehl <broehl@gmail.com>
2025-06-30 06:55:58 -0400

    Toggling a light

---
+++ Assets/_Conestoga Multiplayer/Scripts/LightController.cs
@@ -0,0 +1,23 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class LightController : NetworkBehaviour
+{
+    Light lightSource;
+
+    NetworkVariable<bool> lightState = new NetworkVariable<bool>();
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        lightSource = GetComponent<Light>();
+        lightState.OnValueChanged += UpdateLightState;
+        if (IsOwner) lightState.Value = lightSource.enabled;
+        print("Hello");
+    }
+
+    void UpdateLightState(bool _, bool newvalue) => lightSource.enabled = newvalue;
+
+    [Rpc(SendTo.Server)]
+    public void ToggleRpc() => lightState.Value = !lightState.Value;
+}
