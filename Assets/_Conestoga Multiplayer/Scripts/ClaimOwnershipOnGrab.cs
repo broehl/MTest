56e44e768bd6b065b3fa8b73f12ff1fbc9160e65
Bernie Roehl <broehl@gmail.com>
2025-06-30 08:04:38 -0400

    Update ClaimOwnershipOnGrab.cs

--- Assets/_Conestoga Multiplayer/Scripts/ClaimOwnershipOnGrab.cs
+++ Assets/_Conestoga Multiplayer/Scripts/ClaimOwnershipOnGrab.cs
@@ -4,12 +4,17 @@ public class ClaimOwnershipOnGrab : NetworkBehaviour
 {
     public void OnSelect()
     {
-        if (IsClient && !IsOwner) RequestOwnershipRpc(NetworkManager.Singleton.LocalClientId);
+        if (IsClient && !IsOwner)
+        {
+            print($"Requesting ownership by {NetworkManager.Singleton.LocalClientId}");
+            RequestOwnershipRpc(NetworkManager.Singleton.LocalClientId);
+        }
     }
 
     [Rpc(SendTo.Server)]
     void RequestOwnershipRpc(ulong newClientId)
     {
+        print($"Got ownership change request from {newClientId}");
         GetComponent<NetworkObject>().ChangeOwnership(newClientId);
     }
 }
