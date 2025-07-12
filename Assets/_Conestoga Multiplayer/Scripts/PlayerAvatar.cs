d3a93b6c69b64be7bfce2992041389842cb650bc
Bernie Roehl <broehl@gmail.com>
2025-07-12 16:30:56 -0400

    Update PlayerAvatar.cs

--- Assets/_Conestoga Multiplayer/Scripts/PlayerAvatar.cs
+++ Assets/_Conestoga Multiplayer/Scripts/PlayerAvatar.cs
@@ -36,6 +36,7 @@ namespace ConestogaMultiplayer
             if (IsOwner) networkedAvatarNumber.Value = ((int)NetworkManager.Singleton.LocalClientId) % avatarPrefabs.Length;
             UpdateAvatarNumber(-1, networkedAvatarNumber.Value);
             if (IsOwner) SetPlayerHeight();
+            else ResizeAvatar();
         }
 
         private void UpdateAvatarNumber(int previousValue, int newValue)
@@ -59,7 +60,7 @@ namespace ConestogaMultiplayer
             GameObject avatarRoot = Instantiate(avatarPrefab);
             refs = avatarRoot.GetComponent<AvatarReferences>();
             avatarHeight = refs.headBone.position.y;
-            if (IsOwner && behead) refs.headBone.localScale = Vector3.zero;
+            if (IsOwner && behead) avatarRoot.GetComponent<Animator>().GetBoneTransform(HumanBodyBones.Head).localScale = Vector3.zero;
             playerAvatarChangedEvent.Invoke(playerAvatar);
             return avatarRoot;
         }
